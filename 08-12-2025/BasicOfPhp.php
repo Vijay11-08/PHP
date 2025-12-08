e463ac6f81812ca49749fdd9e8a33303e3eb1748
Otaradi_Vijay_1108 <vijayotaradi11@gmail.com>
2025-12-08 08:59:34 +0530

    Basic of php

---
+++ 08-12-2025/BasicOfPhp.php
@@ -0,0 +1,28 @@
+<!--
+
+What is PHP?
+
+- The PHP Hypertext Preprocessor (PHP) is a server side scripting language that allows web developers to create dynamic content that interacts with databases.
+- PHP is basically used for developing web based software applications.
+- PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.
+- PHP is a server side scripting language that is embedded in HTML.
+- It is integrated with a number of popular databases, including MySQL, PostgreSQL, Oracle, Sybase, Informix, and Microsoft SQL Server. 
+
+-->
+
+<?php   // Syntax of PHP
+echo "Hello, I am Computer Engineer";   // using echo statement
+
+print  "Hello, I am IT Student";        // using print statement
+?>
+
+
+<?php
+// Single line comment
+#  This is also single line comment
+
+
+/*
+multiple-lines comment 
+*/
+?>
